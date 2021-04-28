729767d5621ed85f3c03e9023a64c99d5002fe0b
Matthew <54089351+Matthew78917@users.noreply.github.com>
2021-04-28 11:07:05 -0600

    Add files via upload

---
+++ US.sql
@@ -0,0 +1,56 @@
+--add items to shop
+/* INSERT QUERY NO: 1 */
+INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1419, 'Coalition bottom', 2280, 228
+);
+
+/* INSERT QUERY NO: 2 */
+INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1420, 'Ensign Coalition top', 3420, 342
+);
+
+/* INSERT QUERY NO: 3 */
+INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1421, 'Lieutenant Coalition Top', 5130, 510
+);
+
+/* INSERT QUERY NO: 4 */
+INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1422, 'Major coalition top', 6840, 680
+);
+
+/* INSERT QUERY NO: 5 */
+INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1425, 'Coalition vest', 4080, 408
+);
+
+/* INSERT QUERY NO: 6 */
+INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1426, 'Coalition cap ', 840, 67
+);
+
+/* INSERT QUERY NO: 7 */
+INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1427, 'Coalition helmet', 1380, 110
+);
+
+/* INSERT QUERY NO: 8 */
+INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1428, 'Coalition beret', 1840, 147
+);
