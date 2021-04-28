d0d71679de87e9eb2fd091a5fd6813a10727e084
Matthew <54089351+Matthew78917@users.noreply.github.com>
2021-04-28 11:07:55 -0600

    Add files via upload

--- US.sql
+++ US.sql
@@ -1,55 +1,55 @@
 --add items to shop
-/* INSERT QUERY NO: 1 */
-INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+/* REPLACE QUERY NO: 1 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
 VALUES
 (
 1419, 'Coalition bottom', 2280, 228
 );
 
-/* INSERT QUERY NO: 2 */
-INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+/* REPLACE QUERY NO: 2 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
 VALUES
 (
 1420, 'Ensign Coalition top', 3420, 342
 );
 
-/* INSERT QUERY NO: 3 */
-INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+/* REPLACE QUERY NO: 3 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
 VALUES
 (
 1421, 'Lieutenant Coalition Top', 5130, 510
 );
 
-/* INSERT QUERY NO: 4 */
-INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+/* REPLACE QUERY NO: 4 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
 VALUES
 (
 1422, 'Major coalition top', 6840, 680
 );
 
-/* INSERT QUERY NO: 5 */
-INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+/* REPLACE QUERY NO: 5 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
 VALUES
 (
 1425, 'Coalition vest', 4080, 408
 );
 
-/* INSERT QUERY NO: 6 */
-INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+/* REPLACE QUERY NO: 6 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
 VALUES
 (
 1426, 'Coalition cap ', 840, 67
 );
 
-/* INSERT QUERY NO: 7 */
-INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+/* REPLACE QUERY NO: 7 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
 VALUES
 (
 1427, 'Coalition helmet', 1380, 110
 );
 
-/* INSERT QUERY NO: 8 */
-INSERT INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+/* REPLACE QUERY NO: 8 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
 VALUES
 (
 1428, 'Coalition beret', 1840, 147
