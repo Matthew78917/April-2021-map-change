e2968e7d5e1d8827274394e747464ec533d7f41e
Matthew <54089351+Matthew78917@users.noreply.github.com>
2021-04-28 10:48:15 -0600

    Add files via upload

---
+++ EU.sql
@@ -0,0 +1,233 @@
+--remove from shop
+DELETE FROM `LYHMEut`.`ShopItems` WHERE `ShopItems`.`id` = 1431;
+--add to shop
+/* REPLACE QUERY NO: 1 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+82, 'Chips', 300, 60
+);
+
+/* REPLACE QUERY NO: 2 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+83, 'chocolate bar', 200, 40
+);
+
+/* REPLACE QUERY NO: 3 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+84, 'Candy bar', 150, 30
+);
+
+/* REPLACE QUERY NO: 4 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+85, 'Granola bar', 450, 90
+);
+
+/* REPLACE QUERY NO: 5 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+86, 'Energy Bar', 650, 130
+);
+
+/* REPLACE QUERY NO: 6 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+91, 'Apple juice', 200, 40
+);
+
+/* REPLACE QUERY NO: 7 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+92, 'Grape juice', 200, 40
+);
+
+/* REPLACE QUERY NO: 8 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+105, 'Baseball bat', 890, 133
+);
+
+/* REPLACE QUERY NO: 9 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+115, 'Raw Mauve Berries', 400, 80
+);
+
+/* REPLACE QUERY NO: 10 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+121, 'Military knife', 1490, 223
+);
+
+/* REPLACE QUERY NO: 11 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+270, 'Raw Amber Berries', 500, 100
+);
+
+/* REPLACE QUERY NO: 12 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+271, 'Raw Indigo Berries', 900, 180
+);
+
+/* REPLACE QUERY NO: 13 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+272, 'Raw Jade Berries', 450, 90
+);
+
+/* REPLACE QUERY NO: 14 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+273, 'Raw Russet Berries', 500, 100
+);
+
+/* REPLACE QUERY NO: 15 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+274, 'Raw Teal Berries', 450, 90
+);
+
+/* REPLACE QUERY NO: 16 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+275, 'Raw Vermillion Berries', 450, 90
+);
+
+/* REPLACE QUERY NO: 17 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+333, 'Binoculars', 500, 150
+);
+
+/* REPLACE QUERY NO: 18 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1008, 'Rangefinder', 750, 112
+);
+
+/* REPLACE QUERY NO: 19 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1046, 'Pumpkin', 350, 70
+);
+
+/* REPLACE QUERY NO: 20 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1048, 'Hockey mask', 600, 48
+);
+
+/* REPLACE QUERY NO: 21 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1132, 'Snowball', 1000, 200
+);
+
+/* REPLACE QUERY NO: 22 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1159, 'Maple Syrup', 620, 124
+);
+
+/* REPLACE QUERY NO: 23 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1162, 'Dough', 150, 30
+);
+
+/* REPLACE QUERY NO: 24 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1419, 'Coalition bottom', 2280, 228
+);
+
+/* REPLACE QUERY NO: 25 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1420, 'Ensign Coalition top', 3420, 342
+);
+
+/* REPLACE QUERY NO: 26 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1421, 'Lieutenant Coalition Top', 5130, 510
+);
+
+/* REPLACE QUERY NO: 27 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1422, 'Major coalition top', 6840, 680
+);
+
+/* REPLACE QUERY NO: 28 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1425, 'Coalition vest', 4080, 408
+);
+
+/* REPLACE QUERY NO: 29 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1426, 'Coalition cap ', 840, 67
+);
+
+/* REPLACE QUERY NO: 30 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1427, 'Coalition helmet', 1380, 110
+);
+
+/* REPLACE QUERY NO: 31 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1428, 'Coalition beret', 1840, 147
+);
+
+/* REPLACE QUERY NO: 32 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1437, 'Quadbarrel Blueprint', 5000, 500
+);
+
+/* REPLACE QUERY NO: 33 */
+REPLACE INTO `LYHMEut`.`ShopItems` (`id`, `itemname`, `cost`, `buyback`)
+VALUES
+(
+1439, 'Bayonet Blueprint', 5000, 500
+);
